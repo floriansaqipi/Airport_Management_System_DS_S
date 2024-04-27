1172b27248218a0fbb72ae5b03faeb853aa44bb8
ArdiZariqi <zariqiardi@gmail.com>
2024-04-27 21:20:51 +0200

    update-aircraftmapper-to-handle-flights-and-maintenances-lists

--- src/main/java/com/internationalairport/airportmanagementsystem/dtos/PostAircraftDto.java
+++ src/main/java/com/internationalairport/airportmanagementsystem/dtos/PostAircraftDto.java
@@ -1,9 +1,13 @@
 package com.internationalairport.airportmanagementsystem.dtos;
 
+import java.util.List;
+
 public record PostAircraftDto(
         String tailNumber,
         String model,
         Integer capacity,
-        Integer airlineId
+        Integer airlineId,
+        List<Integer> flights,
+        List<Integer> maintenances
 ) {
 }
