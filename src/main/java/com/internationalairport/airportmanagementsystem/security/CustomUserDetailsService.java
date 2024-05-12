9c3c4897d0df7b9bfa434e5c55cc8d2302739ec6
Gentrit <gentikryeziu851@gmail.com>
2024-05-12 05:23:37 +0200

    added-methods-needed-in-user-&-role-repos-&-updated-securityconfig-file

---
+++ src/main/java/com/internationalairport/airportmanagementsystem/security/CustomUserDetailsService.java
@@ -0,0 +1,38 @@
+package com.internationalairport.airportmanagementsystem.security;
+
+import com.internationalairport.airportmanagementsystem.daos.UserEntityRepository;
+import com.internationalairport.airportmanagementsystem.entities.Role;
+import com.internationalairport.airportmanagementsystem.entities.UserEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class CustomUserDetailsService  implements UserDetailsService {
+
+    private UserEntityRepository userRepository;
+
+    @Autowired
+    public CustomUserDetailsService(UserEntityRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        UserEntity user = userRepository.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException("Username not found"));
+        return new User(user.getUsername(), user.getPassword(), mapRolesToAuthorities(user.getRoles()));
+    }
+
+    private Collection<GrantedAuthority> mapRolesToAuthorities(List<Role> roles) {
+        return roles.stream().map(role -> new SimpleGrantedAuthority(role.getRoleName())).collect(Collectors.toList());
+    }
+}
