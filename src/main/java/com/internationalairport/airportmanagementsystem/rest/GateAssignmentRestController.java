8dea44648473137c59fb56588ca2ce805b29895d
ArditGjyrevci <arditgjyrevci0298@gmail.com>
2024-05-26 15:57:19 +0200

    added-documentation-for-half-of-the-controllers

--- src/main/java/com/internationalairport/airportmanagementsystem/rest/GateAssignmentRestController.java
+++ src/main/java/com/internationalairport/airportmanagementsystem/rest/GateAssignmentRestController.java
@@ -4,6 +4,8 @@ import com.internationalairport.airportmanagementsystem.dtos.post.PostGateAssign
 import com.internationalairport.airportmanagementsystem.dtos.put.PutGateAssignmentDto;
 import com.internationalairport.airportmanagementsystem.entities.GateAssignment;
 import com.internationalairport.airportmanagementsystem.service.interfaces.GateAssignmentService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -20,32 +22,121 @@ public class GateAssignmentRestController {
         this.gateAssignmentService = gateAssignmentService;
     }
 
+    @Operation(
+            description = "Endpoint to get all gate assignments",
+            summary = "Retrieve all gate assignments",
+            responses = {
+                    @ApiResponse(
+                            description = "Successfully retrieved all gate assignments",
+                            responseCode = "200"
+                    ),
+                    @ApiResponse(
+                            description = "Access unauthorized",
+                            responseCode = "401"
+                    )
+            }
+    )
     @GetMapping("/public/gate_assignments")
     public List<GateAssignment> findAllGateAssignments() {
         return gateAssignmentService.findAll();
     }
 
+    @Operation(
+            description = "Endpoint to get a gate assignment by ID",
+            summary = "Retrieve a gate assignment by ID",
+            responses = {
+                    @ApiResponse(
+                            description = "Successfully retrieved the gate assignment",
+                            responseCode = "200"
+                    ),
+                    @ApiResponse(
+                            description = "Gate Assignment ID not found",
+                            responseCode = "404"
+                    )
+            }
+    )
     @GetMapping("/public/gate_assignments/{gateAssignmentId}")
     public GateAssignment getGateAssignmentById(@PathVariable int gateAssignmentId) {
         return gateAssignmentService.findById(gateAssignmentId);
     }
 
+    @Operation(
+            description = "Endpoint to add a new gate assignment",
+            summary = "Add a new gate assignment",
+            responses = {
+                    @ApiResponse(
+                            description = "Successfully added the gate assignment",
+                            responseCode = "200"
+                    ),
+                    @ApiResponse(
+                            description = "Access unauthorized",
+                            responseCode = "401"
+                    )
+            }
+    )
     @PostMapping("/private/gate_assignments")
     public GateAssignment addGateAssignment(@RequestBody PostGateAssignmentDto postGateAssignmentDto) {
         return gateAssignmentService.save(postGateAssignmentDto);
     }
 
+
+    @Operation(
+            description = "Endpoint to update a gate assignment",
+            summary = "Update a gate assignment",
+            responses = {
+                    @ApiResponse(
+                            description = "Successfully updated the gate assignment",
+                            responseCode = "200"
+                    ),
+                    @ApiResponse(
+                            description = "Access unauthorized",
+                            responseCode = "401"
+                    )
+            }
+    )
     @PutMapping("/private/gate_assignments")
     public GateAssignment updateGateAssignment(@RequestBody PutGateAssignmentDto putGateAssignmentDto) {
         return gateAssignmentService.save(putGateAssignmentDto);
     }
 
+    @Operation(
+            description = "Endpoint to delete a gate assignment by ID",
+            summary = "Delete a gate assignment by ID",
+            responses = {
+                    @ApiResponse(
+                            description = "Successfully deleted the gate assignment",
+                            responseCode = "200"
+                    ),
+                    @ApiResponse(
+                            description = "Access unauthorized",
+                            responseCode = "401"
+                    ),
+                    @ApiResponse(
+                            description = "Gate Assignment ID not found",
+                            responseCode = "404"
+                    )
+            }
+    )
     @DeleteMapping("/private/gate_assignments/{gateAssignmentId}")
     public String deleteGateAssignmentById(@PathVariable int gateAssignmentId) {
         gateAssignmentService.deleteById(gateAssignmentId);
         return "Deleted gate assignment id - " + gateAssignmentId;
     }
 
+    @Operation(
+            description = "Endpoint to delete all gate assignments",
+            summary = "Delete all gate assignments",
+            responses = {
+                    @ApiResponse(
+                            description = "Successfully deleted all gate assignments",
+                            responseCode = "200"
+                    ),
+                    @ApiResponse(
+                            description = "Access unauthorized",
+                            responseCode = "401"
+                    )
+            }
+    )
     @DeleteMapping("/private/gate_assignments")
     public String deleteAllGateAssignments() {
         return gateAssignmentService.deleteAll();
--- src/main/java/com/internationalairport/airportmanagementsystem/rest/SecurityCheckpointRestController.java
+++ src/main/java/com/internationalairport/airportmanagementsystem/rest/SecurityCheckpointRestController.java
@@ -4,6 +4,8 @@ import com.internationalairport.airportmanagementsystem.dtos.post.PostSecurityCh
 import com.internationalairport.airportmanagementsystem.dtos.put.PutSecurityCheckpointDto;
 import com.internationalairport.airportmanagementsystem.entities.SecurityCheckpoint;
 import com.internationalairport.airportmanagementsystem.service.interfaces.SecurityCheckpointService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -20,11 +22,43 @@ public class SecurityCheckpointRestController {
         securityCheckpointService=theSecurityCheckpointService;
     }
 
+    @Operation(
+            description = "Get all security checkpoints",
+            summary = "Get all security checkpoints",
+            responses = {
+                    @ApiResponse(
+                            description = "Successfully retrieved all security checkpoints",
+                            responseCode = "200"
+                    ),
+                    @ApiResponse(
+                            description = "Access unauthorized",
+                            responseCode = "401"
+                    )
+            }
+    )
     @GetMapping("/security_checkpoints")
     public List<SecurityCheckpoint> findAllSecurityCheckpoints(){
         return securityCheckpointService.findAll();
     }
 
+    @Operation(
+            description = "Get a security checkpoint by ID",
+            summary = "Get a security checkpoint by ID",
+            responses = {
+                    @ApiResponse(
+                            description = "Successfully retrieved the security checkpoint",
+                            responseCode = "200"
+                    ),
+                    @ApiResponse(
+                            description = "Security checkpoint ID not found",
+                            responseCode = "404"
+                    ),
+                    @ApiResponse(
+                            description = "Access unauthorized",
+                            responseCode = "401"
+                    )
+            }
+    )
     @GetMapping("/security_checkpoints/{securityCheckpointId}")
     public SecurityCheckpoint getSecurityCheckpointById(@PathVariable int securityCheckpointId){
         SecurityCheckpoint securityCheckpoint=securityCheckpointService.findById(securityCheckpointId);
@@ -34,17 +68,62 @@ public class SecurityCheckpointRestController {
         return securityCheckpoint;
     }
 
+    @Operation(
+            description = "Add a new security checkpoint",
+            summary = "Add a new security checkpoint",
+            responses = {
+                    @ApiResponse(
+                            description = "Successfully added the security checkpoint",
+                            responseCode = "200"
+                    ),
+                    @ApiResponse(
+                            description = "Access unauthorized",
+                            responseCode = "401"
+                    )
+            }
+    )
     @PostMapping("/security_checkpoints")
     public SecurityCheckpoint addSecurityCheckpoint(@RequestBody PostSecurityCheckpointDto postSecurityCheckpointDto){
         return securityCheckpointService.save(postSecurityCheckpointDto);
     }
 
-    //add mapping for put
+    @Operation(
+            description = "Update a security checkpoint",
+            summary = "Update a security checkpoint",
+            responses = {
+                    @ApiResponse(
+                            description = "Successfully updated the security checkpoint",
+                            responseCode = "200"
+                    ),
+                    @ApiResponse(
+                            description = "Access unauthorized",
+                            responseCode = "401"
+                    )
+            }
+    )
     @PutMapping("/security_checkpoints")
     public SecurityCheckpoint updateSecurityCheckpoint(@RequestBody PutSecurityCheckpointDto putSecurityCheckpointDto){
         return securityCheckpointService.save(putSecurityCheckpointDto);
     }
 
+    @Operation(
+            description = "Delete a security checkpoint by ID",
+            summary = "Delete a security checkpoint by ID",
+            responses = {
+                    @ApiResponse(
+                            description = "Successfully deleted the security checkpoint",
+                            responseCode = "200"
+                    ),
+                    @ApiResponse(
+                            description = "Security checkpoint ID not found",
+                            responseCode = "404"
+                    ),
+                    @ApiResponse(
+                            description = "Access unauthorized",
+                            responseCode = "401"
+                    )
+            }
+    )
     @DeleteMapping("/security_checkpoints/{securityCheckpointId}")
     public String deleteSecurityCheckpointById(@PathVariable int securityCheckpointId){
         SecurityCheckpoint securityCheckpoint = securityCheckpointService.findById(securityCheckpointId);
@@ -55,6 +134,20 @@ public class SecurityCheckpointRestController {
         securityCheckpointService.deleteById(securityCheckpointId);
         return "Deleted Security Checkpoint id - "+securityCheckpointId;
     }
+    @Operation(
+            description = "Delete all security checkpoints",
+            summary = "Delete all security checkpoints",
+            responses = {
+                    @ApiResponse(
+                            description = "Successfully deleted all security checkpoints",
+                            responseCode = "200"
+                    ),
+                    @ApiResponse(
+                            description = "Access unauthorized",
+                            responseCode = "401"
+                    )
+            }
+    )
     @DeleteMapping("/security_checkpoints")
     public String deleteAllSecurityCheckpoints() {
         return securityCheckpointService.deleteAll();
